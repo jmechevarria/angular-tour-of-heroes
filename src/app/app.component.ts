a49487b2f39945c820a432325ec99d247b5b9dba
Jesús Miguel Echevarría Díaz <jmechevarria2015@gmail.com>
2024-02-26 11:17:10 -0600

    - delegate feature display to hero-detail component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeroesComponent } from './heroes/heroes.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @Component({
   selector: 'app-root',
---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Hero } from '../../types/hero';
+import { CommonModule } from '@angular/common';
+import { HEROES } from '../../mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+
+  imports: [CommonModule, HeroDetailComponent],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.scss',
+})
+export class HeroesComponent {
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
